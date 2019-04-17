82bb040377d848ba3cb27e1e03be28cfd5fde7a5
Ricardo <rroque98@gmail.com>
2019-04-17 11:37:40 -0700

    Set up React and CSS

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return <div>Hello, React is working</div>;
+  }
+}
+
+export default App;
